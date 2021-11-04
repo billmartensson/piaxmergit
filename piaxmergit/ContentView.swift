4aeae91e5d5582041a1d1d5bf85ec418cc12c2d1
Bill Mårtensson <bill@magictechnology.se>
2021-11-04 13:35:05 +0100

    Rundade hörn

--- piaxmergit/ContentView.swift
+++ piaxmergit/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             .foregroundColor(Color.white)
             .padding()
             .background(.blue)
+            .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
     }
 }
 
