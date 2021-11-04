59ee53ee824559a64f207e432703a8f0b4e97a1f
Bill Mårtensson <bill@magictechnology.se>
2021-11-04 13:05:55 +0100

    Initial Commit

---
+++ piaxmergit/piaxmergitApp.swift
@@ -0,0 +1,17 @@
+//
+//  piaxmergitApp.swift
+//  piaxmergit
+//
+//  Created by Bill Martensson on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct piaxmergitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
